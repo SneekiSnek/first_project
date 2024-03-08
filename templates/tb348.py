d73a9c90b395ed19b719eff1ec89a79cb7051545
SneekiSnek <andreas.fe@telia.com>
2024-03-08 10:18:09 +0100

    Initial commit

---
+++ templates/tb348.py
@@ -0,0 +1,6 @@
+import os
+print (__file__) 
+#/Users/andreas.fernberger/Desktop/Prog/My Django Stuff/first_project/templates/tb348.py
+
+print (os.path.dirname(__file__)) 
+#/Users/andreas.fernberger/Desktop/Prog/My Django Stuff/first_project/templates
